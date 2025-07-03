7e1afda4232814b8691545ce90572df00bbc1616
Italo <italocarlo@nti.ufal.br>
2025-07-03 13:41:42 -0300

    Feat: Add Resources

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Res,
+  HttpStatus,
 } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
@@ -24,30 +25,38 @@ export class TransactionController {
   ) {
     const createdTransaction =
       await this.transactionService.create(createTransactionDto);
-    res.status(201).send(createdTransaction);
+    res.status(HttpStatus.CREATED).send(createdTransaction);
     return;
   }
 
   @Get()
-  findAll() {
-    return this.transactionService.findAll();
+  async findAll(@Res() res: Response) {
+    const transactions = await this.transactionService.findAll();
+    return res.status(HttpStatus.OK).send(transactions);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
+  async findOne(@Param('id') id: string, @Res() res: Response) {
+    const foundTransaction = await this.transactionService.findOne(id);
+    return res.status(HttpStatus.OK).send(foundTransaction);
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updateTransactionDto: UpdateTransactionDto,
+    @Res() res: Response,
   ) {
-    return this.transactionService.update(+id, updateTransactionDto);
+    const updatedTransaction = await this.transactionService.update(
+      id,
+      updateTransactionDto,
+    );
+    return res.status(HttpStatus.OK).send(updatedTransaction);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    await this.transactionService.remove(id);
+    return res.status(HttpStatus.NO_CONTENT).send();
   }
 }
--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
@@ -19,19 +19,41 @@ export class TransactionService {
     return createdTransaction;
   }
 
-  findAll() {
-    return `This action returns all transaction`;
+  async findAll() {
+    const transactions = await this.prisma.transaction.findMany();
+    return transactions;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} transaction`;
+  async findOne(id: string) {
+    const foundTransaction = await this.prisma.transaction.findUnique({
+      where: { id },
+    });
+    return foundTransaction;
   }
 
-  update(id: string, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+  async update(id: string, updateTransactionDto: UpdateTransactionDto) {
+    const foundTransaction = await this.findOne(id);
+
+    if (!foundTransaction) {
+      throw new BadRequestException(`Transaction with id ${id} not found`);
+    }
+
+    const updatedTransaction = await this.prisma.transaction.update({
+      where: { id },
+      data: updateTransactionDto,
+    });
+    return updatedTransaction;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} transaction`;
+  async remove(id: string) {
+    const foundTransaction = await this.findOne(id);
+
+    if (!foundTransaction) {
+      throw new BadRequestException(`Transaction with id ${id} not found`);
+    }
+
+    await this.prisma.transaction.delete({
+      where: { id },
+    });
   }
 }
